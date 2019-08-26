705d11375b3b19d4a945ffc888e5a7838d7d7766
Ivan Hernandez <ivan.hl.mail@gmail.com>
2019-08-26 10:36:49 -0500

    Para subir a GitHub

--- MiPrimerWebApiM3/Contexts/ApplicationDbContext.cs
+++ MiPrimerWebApiM3/Contexts/ApplicationDbContext.cs
@@ -17,5 +17,17 @@ namespace MiPrimerWebApiM3.Contexts
         }
         // Autores es la tabla de la base de datos, Autor es la clase
         public DbSet<Autor> Autores { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var autores = new List<Autor>()
+            {
+                new Autor(){Id = 1, Nombre = "Felipe Gavilán" },
+                new Autor(){Id = 2, Nombre = "Claudia Rodríguez" }
+            };
+
+            modelBuilder.Entity<Autor>().HasData(autores);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ MiPrimerWebApiM3/Controllers/AutoresController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiPrimerWebApiM3.Contexts;
+using MiPrimerWebApiM3.Entities;
+
+namespace MiPrimerWebApiM3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IClaseB claseB;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/autores/5 ó GET api/autores/5/felipe
+        [HttpGet("{id}/{param2=Gavilan}")]
+        public async Task<ActionResult<Autor>> Get(int id, string param2)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return Ok(autor);
+        }
+
+        // POST api/autores
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+}
